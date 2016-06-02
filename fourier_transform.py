a05dedb8a402599650c8d4c270e1b1d36f03912f
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2016-06-02 10:23:15 +0900

    Introduced fitting of tail in FFT to imaginary time

--- fourier_transform.py
+++ fourier_transform.py
@@ -3,6 +3,7 @@ from scipy import interpolate
 import scipy
 import sys
 from lib import *
+from scipy.optimize import curve_fit
 
 def mpow(mat, n):
     assert n>=1
@@ -10,31 +11,6 @@ def mpow(mat, n):
         return mat
     else:
         return np.dot(mat,mpow(mat,n-1))
-
-#def ft_to_tau_hyb(ndiv_tau, beta, matsubara_freq, tau, c1, c2, c3, data_n, data_tau,cutoff):
- #nflavor = c1.shape[0]
- #tail correction
- #Htmp = - np.dot(H1,np.dot(H1,H1))+2*H1*H2-H3
- #tail = np.zeros((ndiv_tau,nflavor,nflavor),dtype=complex)
- #for im in range(ndiv_tau):
-     #tail[im,:,:] = c1/(1J*matsubara_freq[im])+c2/((1J*matsubara_freq[im])**2)+c3/((1J*matsubara_freq[im])**3)
- #data_rest = data_n - tail
- #assert is_hermitian(c1)
- #assert is_hermitian(c2)
- #assert is_hermitian(c3)
- #for it in range(ndiv_tau+1):
-     #tau_tmp=tau[it]
-     #if it==0:
-         #tau_tmp=1E-4*(beta/ndiv_tau)
-     #if it==ndiv_tau:
-         #tau_tmp=beta-1E-4*(beta/ndiv_tau)
-     #ztmp=np.zeros((nflavor,nflavor),dtype=complex)
-     #for im in range(cutoff):
-         #ztmp+=data_rest[im]*np.exp(-1J*matsubara_freq[im]*tau_tmp)
-     #ztmp=ztmp/beta
-     #data_tau[it,:,:]=ztmp + ztmp.transpose().conjugate() -0.5*c1 +0.25*c2*(-beta+2*tau_tmp) +0.25*c3*(beta-tau_tmp)*tau_tmp
-     #assert is_hermitian(data_tau[it,:,:])
- #return tail, data_rest
 
 def ft_to_tau_hyb(ndiv_tau, beta, matsubara_freq, tau, c1, c2, c3, data_n, data_tau,cutoff):
  nflavor = c1.shape[0]
@@ -61,18 +37,76 @@ def ft_to_tau_hyb(ndiv_tau, beta, matsubara_freq, tau, c1, c2, c3, data_n, data_
      data_tau[it,:,:] += data_tau_conj[it,:,:] -0.5*c1 +0.25*c2*(-beta+2*tau_tmp) +0.25*c3*(beta-tau_tmp)*tau_tmp
  return tail, data_rest
 
-#def G_tau_to_freq(ntau,beta,G_tau):
-    #assert G_tau.shape[0]==G_tau.shape[1]
-    #assert len(G_tau)==ntau+1
-    #nflavor = G_tau.shape[0]
-#
-    ##x = np.zeros((2*ntau,nflavor,nflavor),dtype=complex)
-    #x[0:ntau+1,:,:] = 1.*G_tau[:,:,:]
-    #x[ntau+1,2*ntau,:,:] = 1.*G_tau[:,:,:]
-#
-    #f_freq = to_freq_fermionic_real_field(ntau,beta,ntau,G_tau.real,ntau)+1J*to_freq_fermionic_real_field(ntau,beta,ntau,G_tau.imag,ntau)
-    #assert len(f_freq)==ntau
-    #return f_freq
+
+#estimate tail
+def estimate_tail(ndiv_tau, beta, matsubara_freq, tau, c1_0, data_n):
+  cutoff = int(3*ndiv_tau/4)
+  nflavor = c1_0.shape[0]
+
+  tail = np.zeros((ndiv_tau,nflavor,nflavor),dtype=complex)
+  for im in range(ndiv_tau):
+    tail[im,:,:] = c1_0/(1J*matsubara_freq[im])
+  xdata = matsubara_freq[cutoff:ndiv_tau]
+  ydata = (data_n-tail)[cutoff:ndiv_tau,:,:]
+
+  def tail_func_real(x, c2_real, c3_imag):
+    return - c2_real/x**2 - c3_imag/x**3
+
+  def tail_func_imag(x, c2_imag, c3_real):
+    return - c2_imag/x**2 + c3_real/x**3
+
+  #def tail_func_real(x, c1_imag, c2_real, c3_imag):
+    #return  c1_imag/x - c2_real/x**2 - c3_imag/x**3
+
+  #def tail_func_imag(x, c1_real, c2_imag, c3_real):
+    #return -c1_real/x - c2_imag/x**2 + c3_real/x**3
+
+  #def tail_func(x, a, b, c):
+     #return a/(1J*x)+b/((1J*x)**2)+c/((1J*x)**3)
+
+  #c1 = np.zeros((nflavor,nflavor),dtype=complex)
+  c1 = 1.*c1_0
+  c2 = np.zeros((nflavor,nflavor),dtype=complex)
+  c3 = np.zeros((nflavor,nflavor),dtype=complex)
+  for flavor in xrange(nflavor):
+    for flavor2 in xrange(nflavor):
+
+      p0 = [0.0, 0.0]
+      popt,pcov = curve_fit(tail_func_real, xdata, ydata[:,flavor,flavor2].real, p0)
+      c2[flavor,flavor2] += popt[0]
+      c3[flavor,flavor2] += 1J*popt[1]
+
+      p0 = [0.0, 0.0]
+      popt,pcov = curve_fit(tail_func_imag, xdata, ydata[:,flavor,flavor2].imag, p0)
+      c2[flavor,flavor2] += 1J*popt[0]
+      c3[flavor,flavor2] += popt[1]
+
+  return c1,c2,c3
+
+#Estimate c2, c2 by a fit
+def ft_to_tau_hyb_with_fitted_tail(ndiv_tau, beta, matsubara_freq, tau, c1_0, data_n, data_tau):
+ nflavor = c1_0.shape[0]
+
+ c1, c2, c3 = estimate_tail(ndiv_tau, beta, matsubara_freq, tau, c1_0, data_n)
+
+ #tail correction
+ tail = np.zeros((ndiv_tau,nflavor,nflavor),dtype=complex)
+ for im in range(ndiv_tau):
+     tail[im,:,:] = c1/(1J*matsubara_freq[im])+c2/((1J*matsubara_freq[im])**2)+c3/((1J*matsubara_freq[im])**3)
+ data_rest = data_n - tail
+ assert is_hermitian(c1)
+
+ y = np.zeros((2*ndiv_tau,nflavor,nflavor),dtype=complex)
+ for im in range(ndiv_tau):
+     y[2*im+1,:,:] = data_rest[im,:,:]
+ y *= 1.0/beta
+
+ data_tau[:,:,:] = np.fft.fft(y,axis=0)[0:ndiv_tau+1,:,:]
+ data_tau_conj = data_tau.transpose((0,2,1)).conj()
+ for it in range(ndiv_tau+1):
+     tau_tmp=tau[it]
+     data_tau[it,:,:] += data_tau_conj[it,:,:] -0.5*c1 +0.25*c2*(-beta+2*tau_tmp) +0.25*c3*(beta-tau_tmp)*tau_tmp
+ return tail, data_rest
 
 def G_tau_to_freq(ntau,beta,G_tau):
     assert len(G_tau)==ntau+1
@@ -160,7 +194,7 @@ class FourieTransformer:
         for isbl in xrange(nsbl):
             start = nflavor_sbl*isbl
             end = nflavor_sbl*(isbl+1)
-            tail,rest = ft_to_tau_hyb(ntau, beta, matsubara_freq, tau, self.c1_[isbl,:], self.c2_[isbl,:], self.c3_[isbl,:], hyb[:,start:end,start:end], hyb_tau[:,start:end,start:end], ncut)
+            tail,rest = ft_to_tau_hyb_with_fitted_tail(ntau, beta, matsubara_freq, tau, self.c1_[isbl,:], hyb[:,start:end,start:end], hyb_tau[:,start:end,start:end])
             high_freq_tail[:,start:end,start:end] = 1.*tail
             hyb_rest[:,start:end,start:end] = 1.*rest
         return hyb_tau, high_freq_tail, hyb_rest
@@ -173,7 +207,7 @@ class FourieTransformer:
             matsubara_freq[im]=((2*im+1)*np.pi)/beta
         for it in range(ntau+1):
             tau[it]=(beta/ntau)*it
-        high_freq_tail,G_rest = ft_to_tau_hyb(ntau, beta, matsubara_freq, tau, self.c1_G_, self.c2_G_, self.c3_G_, G, G_tau, ncut)
+        high_freq_tail,G_rest = ft_to_tau_hyb_with_fitted_tail(ntau, beta, matsubara_freq, tau, self.c1_G_, G, G_tau)
         return G_tau, high_freq_tail, G_rest
 
 def to_freq_bosonic_real_field(ndiv_tau_smpl,beta,n_freq,f_tau,cutoff_rest):
--- test.py
+++ test.py
@@ -4,9 +4,11 @@ import fourier_transform as ft
 nf = 2
 
 beta = 10.0
-ntau = 2000
+ntau = 10
 E = np.array([0.2, 0.5])
-assert len(E)==nf
+c1 = np.array([[1.0, 0.0],[0.0,2.0]],dtype=complex)
+c2 = np.array([[0.2, 0.1+0.1J],[0.1-0.1J,0.5]],dtype=complex)
+c3 = 0.1*c2
 
 theta = 0.2*np.pi
 c = np.cos(theta)
@@ -20,58 +22,74 @@ tau = np.linspace(0,beta,ntau+1)
 Gomega = np.zeros((ntau,nf,nf),dtype=complex)
 Gtau = np.zeros((ntau+1,nf,nf),dtype=complex)
 
-for flavor in xrange(nf):
-    Gomega[:,flavor,flavor] = 1/(1J*omega_n-E[flavor])
-    Gtau[:,flavor,flavor] = -np.exp((beta-tau)*E[flavor])/(1+np.exp(beta*E[flavor]))
+for im in xrange(ntau):
+    Gomega[im,:,:] = c1/(1J*omega_n[im])+c2/((1J*omega_n[im])**2)+c3/((1J*omega_n[im])**3)
 
-for i in xrange(ntau+1):
-    Gtau[i,:,:] = np.dot(np.dot(Umat.conjugate().transpose(),Gtau[i,:,:]),Umat)
+#for i in xrange(ntau+1):
+    #Gtau[i,:,:] = np.dot(np.dot(Umat.conjugate().transpose(),Gtau[i,:,:]),Umat)
 
-for i in xrange(ntau):
-    Gomega[i,:,:] = np.dot(np.dot(Umat.conjugate().transpose(),Gomega[i,:,:]),Umat)
+#for i in xrange(ntau):
+    #Gomega[i,:,:] = np.dot(np.dot(Umat.conjugate().transpose(),Gomega[i,:,:]),Umat)
 
-c1 = np.diag([1+0.0J]*nf)
-c2 = np.zeros((nf,nf),dtype=complex)
-c3 = np.zeros((nf,nf),dtype=complex)
+#c1 = np.diag([1+0.0J]*nf)
+#c2 = np.zeros((nf,nf),dtype=complex)
+#c3 = np.zeros((nf,nf),dtype=complex)
+#
+#Gtau_fft = np.zeros_like(Gtau)
+#ft.ft_to_tau_hyb(ntau, beta, omega_n, tau, c1, c2, c3, Gomega, Gtau_fft, ntau/2)
+#
+#Gomega_fft = ft.G_tau_to_freq2(ntau,beta,Gtau,ntau/2)
+#
+#f = open('G.txt', 'w')
+#for flavor in xrange(nf):
+   #for flavor2 in xrange(nf):
+      #for i in xrange(ntau+1):
+         #print>>f, i, Gtau[i,flavor,flavor2].real, Gtau[i,flavor,flavor2].imag
+      #print>>f, ""
+      #print>>f, ""
+#f.close()
+#
+#f = open('G-fft.txt', 'w')
+#for flavor in xrange(nf):
+   #for flavor2 in xrange(nf):
+      #for i in xrange(ntau+1):
+         #print>>f, i, Gtau_fft[i,flavor,flavor2].real, Gtau_fft[i,flavor,flavor2].imag
+      #print>>f, ""
+      #print>>f, ""
+#f.close()
+#
+#f = open('Gomega.txt', 'w')
+#for flavor in xrange(nf):
+   #for flavor2 in xrange(nf):
+      #for i in xrange(ntau):
+         #print>>f, i, Gomega[i,flavor,flavor2].real, Gomega[i,flavor,flavor2].imag
+      #print>>f, ""
+      #print>>f, ""
+#f.close()
+#
 
-Gtau_fft = np.zeros_like(Gtau)
-ft.ft_to_tau_hyb(ntau, beta, omega_n, tau, c1, c2, c3, Gomega, Gtau_fft, ntau/2)
+#f = open('Gomega-fft.txt', 'w')
+#for flavor in xrange(nf):
+   #for flavor2 in xrange(nf):
+      #for i in xrange(ntau):
+         #print>>f, i, Gomega_fft[i,flavor,flavor2].real, Gomega_fft[i,flavor,flavor2].imag
+      #print>>f, ""
+      ##print>>f, ""
+#f.close()
 
-Gomega_fft = ft.G_tau_to_freq2(ntau,beta,Gtau,ntau/2)
-
+##### TEST ESTIMATE TAIL ######
+c1_fit, c2_fit, c3_fit = ft.estimate_tail(ntau, beta, omega_n, tau, c1, Gomega)
+#print c3
+#print c3_fit
+print np.abs(c1-c1_fit)
+print np.abs(c2-c2_fit)
+print np.abs(c3-c3_fit)
+ft.ft_to_tau_hyb_with_fitted_tail(ntau, beta, omega_n, tau, c1, Gomega, Gtau)
 f = open('G.txt', 'w')
 for flavor in xrange(nf):
    for flavor2 in xrange(nf):
       for i in xrange(ntau+1):
          print>>f, i, Gtau[i,flavor,flavor2].real, Gtau[i,flavor,flavor2].imag
-      print>>f, ""
-      print>>f, ""
-f.close()
-
-f = open('G-fft.txt', 'w')
-for flavor in xrange(nf):
-   for flavor2 in xrange(nf):
-      for i in xrange(ntau+1):
-         print>>f, i, Gtau_fft[i,flavor,flavor2].real, Gtau_fft[i,flavor,flavor2].imag
-      print>>f, ""
-      print>>f, ""
-f.close()
-
-f = open('Gomega.txt', 'w')
-for flavor in xrange(nf):
-   for flavor2 in xrange(nf):
-      for i in xrange(ntau):
-         print>>f, i, Gomega[i,flavor,flavor2].real, Gomega[i,flavor,flavor2].imag
-      print>>f, ""
-      print>>f, ""
-f.close()
-
-
-f = open('Gomega-fft.txt', 'w')
-for flavor in xrange(nf):
-   for flavor2 in xrange(nf):
-      for i in xrange(ntau):
-         print>>f, i, Gomega_fft[i,flavor,flavor2].real, Gomega_fft[i,flavor,flavor2].imag
       print>>f, ""
       print>>f, ""
 f.close()
