4cf88b2ee9d766301d68047c94bb9cc406fe5011
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2016-08-12 14:15:15 +0900

    Added generate_U_tensor_SK_density

--- Uijkl.py
+++ Uijkl.py
@@ -347,11 +347,22 @@ def generate_U_tensor_SK(n_orb, U, JH):
 
     return U_tensor.reshape((2*n_orb,2*n_orb,2*n_orb,2*n_orb))
 
-#n_site = 3
-#Uval = 1.0
-#Jval = 0.25*Uval
-#
-#V_mat = np.identity(2*n_site,dtype=complex)
-#
-#U_tensor_site_basis = generate_U_tensor_SK(n_site, Uval, Jval)
-#gen_Uijkl(U_tensor_site_basis, V_mat, 'Uijkl.txt')
+#Order of operators: c^\dagger_{iorb1} c^\dagger_{iorb2} c_{iorb3} c_{iorb4}
+def generate_U_tensor_SK_density(n_orb, U, JH):
+    U_tensor = np.zeros((n_orb,2,n_orb,2,n_orb,2,n_orb,2),dtype=complex)
+
+    for iorb1 in xrange(n_orb):
+        for iorb2 in xrange(n_orb):
+            for iorb3 in xrange(n_orb):
+                for iorb4 in xrange(n_orb):
+                    coeff = 0.0
+                    if iorb1==iorb2 and iorb2==iorb3 and iorb3==iorb4:
+                        coeff = U
+                    elif iorb1==iorb4 and iorb2==iorb3 and iorb1!=iorb2:
+                        coeff = U-2*JH
+
+                    for isp in xrange(2):
+                        for isp2 in xrange(2):
+                            U_tensor[iorb1,isp,    iorb2,isp2,    iorb3,isp2,  iorb4,isp] += 0.5*coeff
+
+    return U_tensor.reshape((2*n_orb,2*n_orb,2*n_orb,2*n_orb))
